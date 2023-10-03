c205055927cd6b59d7825323b0666c493423e157
thewickest <graubuntu@gmail.com>
2023-10-03 12:43:02 +0200

    Mapping Through Data with Hooks
    We map an array of data with the .map function and print it as a select array

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit"]
+
 
 const SearchParams = () => {
-  
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -13,6 +15,18 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"/>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}>
+            <option/>
+            {ANIMALS.map((animal) =>
+              <option key={animal}>{animal}</option>
+            )}
+          </select>
         </label>
         <button >Submit</button>
       </form>
